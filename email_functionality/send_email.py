c9239249df80a6ac3a02930d9d9728387cc71c91
Cole Gaito <gaitocole@Cole-Gaitos-MBP.hsd1.co.comcast.net>
2022-04-09 12:18:51 -0600

    Initial GMAIL script creation

---
+++ email_functionality/send_email.py
@@ -0,0 +1,21 @@
+#For use for CU CS3308 Student project
+import smtplib
+
+#creating a SMTP session
+s = smtplib.SMTP('smtp.gmail.com', 587)
+
+#start TLS for security
+s.starttls()
+
+#Authentication
+s.login("cs3308tasklist@gmail.com", "CUSchoolProject")
+
+#Message to be sent
+message = "Message to be sent"
+
+#send the email sender/receiver
+s.sendmail("cs3308tasklist@gmail.com", "coga6899@colorado.edu")
+
+#end session
+s.quit
+
