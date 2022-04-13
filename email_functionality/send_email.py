68d1dcbdd0af9723e21858a999d52b396d386ae4
Cole Gaito <gaitocole@Cole-Gaitos-MBP.Home>
2022-04-12 19:55:45 -0600

    Updated Email so now it sends, though need to write additional functionality so you can insert email addresss and messages externally.

--- email_functionality/send_email.py
+++ email_functionality/send_email.py
@@ -1,24 +1,42 @@
-#usr/bin/python3
+#!/usr/bin/python3
+
+#tmobile @tmomail.net
+#ATT 	 @txt.att.net
+#Verizon @vtext.com
+
 #For use for CU CS3308 Student project
 import smtplib
+import emailconfig
 
-#creating a SMTP session
-s = smtplib.SMTP('smtp.gmail.com', 587)
+def send_email(subject, msg):
+	try:
+		#creating a SMTP session
+		s = smtplib.SMTP('smtp.gmail.com:587')
+		
+		s.ehlo()
 
-#start TLS for security
-s.starttls()
+		#start TLS for security
+		s.starttls()
 
-#Authentication
-s.login("cs3308tasklist@gmail.com", "CUSchoolProject")
+		#Authentication
+		#s.login(emailconfig.Email_Address, emailconfig.Password)
+		s.login("cs3308tasklist@gmail.com", "CUSchoolProject")
 
-#Message to be sent
-message = "Message to be sent"
+		#Message to be sent
+		message = 'Subject: {}\n\n{}'.format(subject, msg)		
 
-#send the email sender/receiver
-s.sendmail("cs3308tasklist@gmail.com", "coga6899@colorado.edu")
+		#send the email sender/receiver
+		s.sendmail("cs3308tasklist@gmail.com", "cs3308tasklist@gmail.com", message)
 
-#end session
-s.quit
+		#end session
+		s.quit()
+		print("Success Email sent")
 
-#Print Confirmation of email sent
-print('Mail Sent')
+	except:
+		print("Email failed to send.")
+
+
+subject = "Test subject"
+msg = "Hello this is a test of the email system!"
+
+send_email(subject, msg)
