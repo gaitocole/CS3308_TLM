54f5823ddf5f7540321cb0cc6670a2fbf6444c1a
Cole Gaito <gaitocole@Cole-Gaitos-MBP.Home>
2022-04-12 18:12:33 -0600

    Adding additional commit to build out Heroku

--- email_functionality/send_email.py
+++ email_functionality/send_email.py
@@ -1,3 +1,4 @@
+#usr/bin/python3
 #For use for CU CS3308 Student project
 import smtplib
 
@@ -19,3 +20,5 @@ s.sendmail("cs3308tasklist@gmail.com", "coga6899@colorado.edu")
 #end session
 s.quit
 
+#Print Confirmation of email sent
+print('Mail Sent')
